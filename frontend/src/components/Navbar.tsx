b5c7a58af137c0b61489a43a97514622def98e7a
CodeforNitin <codeforni3@gmail.com>
2024-06-29 23:58:39 +0530

    user buttons for login/logout/admin

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,13 @@
 import { NavLink } from 'react-router-dom'
 import MaxWidthWrapper from './MaxWidthWrapper'
+import { buttonVariants } from './ui/button'
+import { ArrowRight } from 'lucide-react'
 
 const Navbar = () => {
+
+    const user = undefined;
+    const isAdmin:Boolean = false;
+
   return (
     <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
       <MaxWidthWrapper>
@@ -9,6 +15,71 @@ const Navbar = () => {
           <NavLink to='/' className='font-sans flex z-40 font-bold tracking-wider items-center'>
             Cover<span className='text-green-600 font-extrabold text-2xl text-balance !leading-tight'>Z</span>
           </NavLink>
+          <div className='h-full flex items-center space-x-4'>
+            {user ? (
+              <>
+                <NavLink
+                  to='/api/auth/logout'
+                  className={buttonVariants({
+                    size: 'sm',
+                    variant: 'ghost',
+                  })}>
+                  Sign out
+                </NavLink>
+                {isAdmin ? (
+                  <NavLink
+                    to='/dashboard'
+                    className={buttonVariants({
+                      size: 'sm',
+                      variant: 'ghost',
+                    })}>
+                    Dashboard ✨
+                  </NavLink>
+                ) : null}
+                <NavLink
+                  to='/configure/upload'
+                  className={buttonVariants({
+                    size: 'sm',
+                    className: 'hidden sm:flex items-center gap-1',
+                  })}>
+                  Create case
+                  <ArrowRight className='ml-1.5 h-5 w-5' />
+                </NavLink>
+              </>
+            ) : (
+              <>
+                <NavLink
+                  to='/api/auth/register'
+                  className={buttonVariants({
+                    size: 'sm',
+                    variant: 'ghost',
+                  })}>
+                  Sign up
+                </NavLink>
+
+                <NavLink
+                  to='/api/auth/login'
+                  className={buttonVariants({
+                    size: 'sm',
+                    variant: 'ghost',
+                  })}>
+                  Login
+                </NavLink>
+
+                <div className='h-8 w-px bg-zinc-200 hidden sm:block' />
+
+                <NavLink
+                  to='/configure/upload'
+                  className={buttonVariants({
+                    size: 'sm',
+                    className: 'hidden sm:flex items-center gap-1',
+                  })}>
+                  Create case
+                  <ArrowRight className='ml-1.5 h-5 w-5' />
+                </NavLink>
+              </>
+            )}
+          </div>
         </div>
       </MaxWidthWrapper>
     </nav>
