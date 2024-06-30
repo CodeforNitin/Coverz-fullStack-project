2fa94ec1a68534ffa7d31b87d4bceb5de51c23ab
CodeforNitin <codeforni3@gmail.com>
2024-06-30 23:06:03 +0530

    setting up mongodb and connecting server to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,19 @@ const cookieParser = require("cookie-parser");
 const cors = require('cors')
 
 
-
 //express app
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(process.env.PORT, ()=>{
-    console.log("listening on port", process.env.PORT);
-})
+//connect to database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listening on port once connection build
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port", process.env.PORT);
+    });
+  })
+  .catch((err) => console.log(err));
