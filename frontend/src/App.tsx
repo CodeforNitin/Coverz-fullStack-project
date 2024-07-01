dc0373e842ffdec708a87e56a3373f496b8f0ed1
CodeforNitin <codeforni3@gmail.com>
2024-07-01 20:17:45 +0530

    extracting user to APP.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,35 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, RouteObject, RouterProvider,Navigate } from "react-router-dom"
+import { useAuthContext } from './hooks/useAuthContext'
 import Home from "./pages/Home";
 import Error from "./pages/Error";
 import { SignupForm } from "./pages/Signup";
 import { LoginForm } from './pages/Login';
 
-const router = createBrowserRouter([
-  {
-    path:"/",
-    element: <Home/>
-  },
-  {
-    path:"/signup",
-    element: <SignupForm/>
-  },
-  {
-    path:"/login",
-    element: <LoginForm/>
-  },
-  {
-    path:"*",
-    element: <Error/>
-  }
-]);
 
 function App() {
+
+  const {user} = useAuthContext();
+  const routes: RouteObject[] = [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/signup",
+      element: <SignupForm />,
+    },
+    {
+      path: "/login",
+      element: <LoginForm />,
+    },
+    {
+      path: "*",
+      element: <Error />,
+    },
+  ];
+
+  const router = createBrowserRouter(routes);
+
   return (
     <>
     <RouterProvider router={router}/>
